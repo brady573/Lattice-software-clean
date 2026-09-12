20f551348f5e9f22fa1277d4fee97ef0b476fd10
brady573 <114089255+brady573@users.noreply.github.com>
2026-09-12 11:28:39 -0600

    Allow structural advisory Recommendation representation

--- migrations/038_recommendation_structural_representation.sql
+++ migrations/038_recommendation_structural_representation.sql
@@ -3,4 +3,4 @@ ALTER TABLE recommendations
 
 ALTER TABLE recommendations
   ADD CONSTRAINT recommendations_representation_kind
-  CHECK (representation_kind IN ('LEGACY_FREEFORM', 'STRUCTURAL_USER_MATERIAL_V1'));
+  CHECK (representation_kind IN ('LEGACY_FREEFORM', 'STRUCTURAL_ADVISORY_V1'));
